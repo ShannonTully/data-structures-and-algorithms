2967964e83ddf0de81df1065812451758e175cd5
ShannonTully <scatman.st@gmail.com>
2018-04-03 21:19:56 -0700

    what am i supposed to do

---
+++ data_structures/queues/fifo_animal_shelter.py
@@ -0,0 +1,32 @@
+from .node import Node
+
+
+class Queue:
+    def __init__(self, iterable=[]):
+        self.front = None
+        self.back = None
+        self._size = 0
+        for item in reversed(iterable):
+            self.enqueue(item)
+
+    def __len__(self):
+        return self._size
+
+    def __str__(self):
+        return self.front
+
+    def enqueue(self, val):
+    try:
+        node = Node(val)
+    except TypeError:
+        print('That value is unacceptable.')
+        exit()
+
+    node._next = self.back
+    self.back = node
+    self._size += 1
+
+    return self.back
+
+    def dequeue(self, pref):
+        pass
