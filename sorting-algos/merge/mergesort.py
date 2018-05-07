40b106c9c4c2e2eb5b1ab48f3491cf327be58b87
Shannon Tully <scatman.st@gmail.com>
2018-05-07 14:24:49 -0700

    done

--- sorting-algos/merge/mergesort.py
+++ sorting-algos/merge/mergesort.py
@@ -1,1 +1,49 @@
 """The merge sorting algorithm."""
+
+
+def mergesort(unsorted):
+    """Take in an unsorted list and sort it recursivly using the merge sort method."""
+    if len(unsorted) < 2:
+        return unsorted
+
+    left = []
+    right = []
+    count = 0
+
+    for item in unsorted:
+        if count < len(unsorted) / 2:
+            left.append(item)
+            count += 1
+        else:
+            right.append(item)
+            count += 1
+
+    left = mergesort(left)
+    right = mergesort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    """Merge helper for the merge sort."""
+    final = []
+    leftcount = 0
+    rightcount = 0
+
+    while leftcount < len(left) and rightcount < len(right):
+        if left[leftcount] < right[rightcount]:
+            final.append(left[leftcount])
+            leftcount += 1
+        else:
+            final.append(right[rightcount])
+            rightcount += 1
+
+    while leftcount < len(left):
+        final.append(left[leftcount])
+        leftcount += 1
+
+    while rightcount < len(right):
+        final.append(right[rightcount])
+        rightcount += 1
+
+    return final
--- sorting-algos/merge/test_mergesort.py
+++ sorting-algos/merge/test_mergesort.py
@@ -1,1 +1,33 @@
 """The tests for the merge sorting algorithm."""
+
+from .mergesort import mergesort
+
+
+def test_basic_mergesort():
+    """Basic mergesort test."""
+    unsorted = [2, 1]
+    assert mergesort(unsorted) == [1, 2]
+
+
+def test_one_element():
+    """One element mergesort test."""
+    unsorted = [1]
+    assert mergesort(unsorted) == [1]
+
+
+def test_less_basic_mergesort():
+    """Less basic mergesort test."""
+    unsorted = [2, 4, 3, 1]
+    assert mergesort(unsorted) == [1, 2, 3, 4]
+
+
+def test_reversed_mergesort():
+    """Basic reversed order mergesort test."""
+    unsorted = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    assert mergesort(unsorted) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+def test_complex_mergesort():
+    """Complex mergesort test."""
+    unsorted = [49, 682, -9000, 62, 8473, -47, 1]
+    assert mergesort(unsorted) == [-9000, -47, 1, 49, 62, 682, 8473]
